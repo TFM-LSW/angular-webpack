f3d194788b0c4d998d3d0840f0f24953fdd6c39b
Lee Walters <lee@leewalters.net>
2017-04-24 17:06:30 +0100

    Added core/ module import guard

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { routing } from '../app.routing';
@@ -6,6 +6,8 @@ import { LoggerService } from './logger.service';
 import { NavComponent } from './nav/nav.component';
 import { SpinnerComponent } from './spinner/spinner.component';
 import { SpinnerService } from './spinner/spinner.service';
+import { throwIfAlreadyLoaded } from './module-import-guard';
+
 @NgModule({
   imports: [
     CommonModule, // we use ngFor
@@ -15,4 +17,8 @@ import { SpinnerService } from './spinner/spinner.service';
   declarations: [NavComponent, SpinnerComponent],
   providers: [LoggerService, SpinnerService]
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor( @Optional() @SkipSelf() parentModule: CoreModule) {
+    throwIfAlreadyLoaded(parentModule, 'CoreModule');
+  }
+}
