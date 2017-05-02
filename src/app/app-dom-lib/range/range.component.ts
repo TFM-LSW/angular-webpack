0e542e28fe9ae4d103ff62bc8f35279589044f10
Lee Walters <lee.walters@momentumft.co.uk>
2017-05-02 12:11:42 +0100

    housekeeping

--- src/app/app-dom-lib/range/range.component.ts
+++ src/app/app-dom-lib/range/range.component.ts
@@ -13,58 +13,14 @@ export class RangeComponent implements OnInit, OnDestroy, OnChanges {
   @Input() step = 10;
   @Input() value;
   @Input() increments;
-  @ViewChild('MFTRangeField') rangeField;
+  @ViewChild('MFTRange') rangeField;
   @Output() rangeChange = new EventEmitter<any>();
-
-  rangeControl = new FormControl();
-  webkitBackgroundSize: any;
-  incrementArray = this.increments;
-
+  
   private subscription: ISubscription;
+  private rangeControl = new FormControl();
+  private webkitBackgroundSize: any;
+  private incrementArray = this.increments;
   constructor() { }
-
-  setWebkitTrack(value) {
-    this.webkitBackgroundSize = (value - this.min) * 100 / (this.max - this.min) + '% 100%';
-  }
-
-  getOffset(el, relativeToViewPort = false) {
-    if (relativeToViewPort === null) { relativeToViewPort = false; }
-    let _x = 0;
-    let _y = 0;
-    while (el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
-      _x += relativeToViewPort ? el.offsetLeft - el.scrollLeft : el.offsetLeft;
-      _y += relativeToViewPort ? el.offsetTop - el.scrollTop : el.offsetTop;
-      el = el.offsetParent;
-    }
-    return {
-      x: _x,
-      y: _y
-    };
-  }
-
-  setValue(value) {
-    this.value = Math.round(value);
-    this.rangeChange.emit(this.value);
-    this.setWebkitTrack(this.value);
-  }
-
-  getIncArr = () => {
-    const createArr = (min: number, max: number) => {
-      const incArr = [];
-      for (let index: any = min; index <= max; index++) {
-        incArr.push(index);
-      }
-      return incArr;
-    };
-    if (typeof this.increments === 'string') {
-      const tmpArr = this.increments.split('-');
-      return createArr(Number(tmpArr[0]), Number(tmpArr[1]));
-    } else if (typeof this.increments === 'boolean') {
-      return createArr(this.min, this.max);
-    } else {
-      return this.increments;
-    }
-  }
 
   @HostListener('touchstart', ['$event'])
   handleTouchStart(e: TouchEvent) {
@@ -89,5 +45,49 @@ export class RangeComponent implements OnInit, OnDestroy, OnChanges {
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
+  }
+
+
+  private setWebkitTrack(value) {
+    this.webkitBackgroundSize = (value - this.min) * 100 / (this.max - this.min) + '% 100%';
+  }
+
+  private getOffset(el, relativeToViewPort = false) {
+    if (relativeToViewPort === null) { relativeToViewPort = false; }
+    let _x = 0;
+    let _y = 0;
+    while (el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
+      _x += relativeToViewPort ? el.offsetLeft - el.scrollLeft : el.offsetLeft;
+      _y += relativeToViewPort ? el.offsetTop - el.scrollTop : el.offsetTop;
+      el = el.offsetParent;
+    }
+    return {
+      x: _x,
+      y: _y
+    };
+  }
+
+  private setValue(value) {
+    this.value = Math.round(value);
+    this.rangeChange.emit(this.value);
+    this.setWebkitTrack(this.value);
+  }
+
+  private getIncArr = () => {
+    const createArr = (min: number, max: number) => {
+      const incArr = [];
+      for (let index: any = min; index <= max; index++) {
+        incArr.push(index);
+      }
+      return incArr;
+    };
+    if (typeof this.increments === 'string') {
+      const tmpArr = this.increments.split('-');
+      return createArr(Number(tmpArr[0]), Number(tmpArr[1]));
+    } else if (typeof this.increments === 'boolean') {
+      return createArr(this.min, this.max);
+    } else {
+      return this.increments;
+    }
   }
 }
