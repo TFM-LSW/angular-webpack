ef9eb90b61f156effa0176b6c09f2e8465c75b78
Lee Walters <lee.walters@momentumft.co.uk>
2017-05-02 09:58:16 +0100

    Missing home range files

---
+++ src/app/home/home.range.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RangeComponent } from '../app-dom-lib/range/range.component';
+
+
+@Component({
+  selector: 'my-range-ext',
+  templateUrl: '../app-dom-lib/range/range.component.html',
+  styleUrls: ['./home.range.component.scss']
+})
+export class HomeRangeComponent extends RangeComponent {
+ constructor() {
+    super();
+ }
+}
